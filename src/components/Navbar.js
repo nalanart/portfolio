570aee1ae786e5647154dab2dec13a8b8cc03f37
nalanart <alantran96@gmail.com>
2021-02-05 17:48:52 -0500

    Added hamburger menu with for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,25 @@
 import './styles/Navbar.css'
+import { useState } from 'react'
 
 const Navbar = (params) => {
+  const [active, setActive] = useState(false)
+
   return (
     <div className="site-navbar__inner">
       <div className="site-navbar__section">
         <a className="site-logo" href="/">ALAN TRAN</a>
       </div>
       <div className="site-navbar__section">
-        <nav className="site-menu">
+        <nav className={active ? "site-menu burger-active" : "site-menu"}>
           <a className="site-menu__item" href="/projects">Projects</a>
           <a className="site-menu__item" href="/about">About</a>
           <a className="site-menu__item" href="/contact">Contact</a>
         </nav>
+      </div>
+      <div className="burger" onClick={() => setActive(!active)}>
+        <div className={active ? "toggle line1" : "line1"}></div>
+        <div className={active ? "toggle line2" : "line2"}></div>
+        <div className={active ? "toggle line3" : "line3"}></div>
       </div>
     </div>
   )
